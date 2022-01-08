8548b24365a27e7b79d067637f150ac960728092
mazuh <marcell-mz@hotmail.com>
2022-01-07 22:35:12 -0300

    enumerate devices and initial form saving on global context

--- src/App.js
+++ src/App.js
@@ -1,26 +1,121 @@
-import logo from './logo.svg';
 import 'webrtc-adapter';
-import './libs/janus'
+import './libs/janus';
 import './App.css';
+import { useContext, useEffect, useState } from 'react';
+import GlobalProvider, { GlobalContext } from './contexts';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <GlobalProvider>
+      <div>
+        <header>
+          <h1>Millennium Eye</h1>
+        </header>
+        <JoinView />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+function JoinView() {
+  const [microphones, setMicrophones] = useState([]);
+  const [cameras, setCameras] = useState([]);
+  const [isLoadingDevices, setLoadingDevices] = useState(true);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then((stream) => {
+        stream.getTracks().forEach((t) => t.stop());
+        return navigator.mediaDevices.enumerateDevices();
+      })
+      .then((allDevices) => {
+        const newMicrophones = allDevices.filter(
+          (d) => d.kind === 'audioinput' && d.deviceId !== 'communications'
+        );
+        setMicrophones(newMicrophones);
+
+        const newCameras = allDevices.filter(
+          (d) => d.kind === 'videoinput' && d.deviceId !== 'communications'
+        );
+        setCameras(newCameras);
+        setLoadingDevices(false);
+      })
+      .catch(() => {
+        setError('You must allow access to your devices and refresh the page.');
+      });
+  }, []);
+
+  const { setDevices, setUsername, setOpponent } = useContext(GlobalContext);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setUsername(event.target.username.value.trim());
+    setOpponent(event.target.opponent.value.trim());
+    setDevices({
+      audio: microphones.find((m) => m.deviceId === event.target.microphone.value) || null,
+      camera: cameras.find((c) => c.deviceId === event.target.camera.value) || null,
+    });
+  };
+
+  if (error) {
+    return (
+      <main>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <strong>Error:</strong> {error}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </main>
+    );
+  }
+
+  if (isLoadingDevices) {
+    return (
+      <main>
+        <p>
+          <em>Loading devices...</em>
+        </p>
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <form onSubmit={handleSubmit}>
+        <label>
+          My username:
+          <input name="username" autoComplete="off" required />
+        </label>
+        <br />
+        <label>
+          Another player's username:
+          <input name="opponent" autoComplete="off" required />
+        </label>
+        <br />
+        <label>
+          Microphone:
+          <select name="microphone" required>
+            {microphones.map((mic) => (
+              <option key={mic.deviceId} value={mic.deviceId}>
+                {mic.label}
+              </option>
+            ))}
+          </select>
+        </label>
+        <br />
+        <label>
+          Camera:
+          <select name="camera" required>
+            {cameras.map((cam) => (
+              <option key={cam.deviceId} value={cam.deviceId}>
+                {cam.label}
+              </option>
+            ))}
+          </select>
+        </label>
+        <br />
+        <button>Duel!</button>
+      </form>
+    </main>
   );
 }
 
---
+++ src/contexts.js
@@ -0,0 +1,35 @@
+import { createContext, useState } from 'react';
+
+export const GlobalContext = createContext();
+
+export default function GlobalProvider({ children }) {
+  const [devices, setDevices] = useState({ audio: null, video: null });
+  const [username, setUsername] = useState('');
+  const [opponent, setOpponent] = useState('');
+  const [callState, setCallState] = useState(STATE_OFF);
+
+  const value = {
+    devices,
+    setDevices,
+    username,
+    setUsername,
+    opponent,
+    setOpponent,
+    callState,
+    setCallState,
+  };
+
+  return <GlobalContext.Provider value={value}>{children}</GlobalContext.Provider>;
+}
+
+export const STATE_OFF = 'OFF';
+export const STATE_CONNECTING = 'CONNECTING';
+export const STATE_CONNECTED = 'CONNECTED';
+export const STATE_CONNECTION_FAILED = 'CONNECTION_FAILED';
+export const STATE_REGISTERING = 'REGISTERING';
+export const STATE_REGISTERED = 'REGISTERED';
+export const STATE_REGISTER_FAILED = 'REGISTER_FAILED';
+export const STATE_CALLING = 'CALLING';
+export const STATE_IN_CALL = 'IN_CALL';
+export const STATE_CALL_FAILED = 'CALL_FAILED';
+export const STATE_CALL_HUNGUP = 'CALL_HUNGUP';
