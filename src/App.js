de5b34ebeda7077236e1d629eb52ab59e97d055c
Rodrigo Muniz <32344098+rcmuniz1994@users.noreply.github.com>
2022-01-24 23:06:48 -0300

    Create call view (#1)
    * create call view
    * create a component to the call view

--- src/App.js
+++ src/App.js
@@ -16,19 +16,10 @@ import GlobalProvider, {
 export default function App() {
   return (
     <GlobalProvider>
-      <div>
-        <header>
-          <h1>Millennium Eye</h1>
-        </header>
-        <JoinView />
-        <footer>
-          <p>
-            <small>
-              <CallStatusIndicator />
-            </small>
-          </p>
-        </footer>
-      </div>
+      <header>
+        <h1>Millennium Eye</h1>
+      </header>
+      <AppView />
     </GlobalProvider>
   );
 }
@@ -45,6 +36,56 @@ function CallStatusIndicator() {
       )}
       <strong>Status:</strong> {callState}
     </span>
+  );
+}
+
+function AppView() {
+  const { callState } = useContext(GlobalContext);
+
+  const mustShowCallView = [STATE_CALLING, STATE_RINGING, STATE_ANSWERING, STATE_IN_CALL].includes(
+    callState
+  );
+  return mustShowCallView ? <CallView /> : <JoinView />;
+}
+
+function CallView() {
+  const { callState, hangup, acceptIncomingCall } = useContext(GlobalContext);
+
+  const isRinging = callState === STATE_RINGING;
+
+  if (isRinging) {
+    return (
+      <main className="call-view">
+        <section className="action-bar">
+          <CallStatusIndicator />
+          <div>
+            <button type="button" onClick={acceptIncomingCall}>
+              Accept call
+            </button>
+            <button type="button" onClick={hangup}>
+              Hangup
+            </button>
+          </div>
+        </section>
+      </main>
+    );
+  }
+
+  return (
+    <main className="call-view">
+      <section className="localstream-area">
+        <video id="local-video" width={400} autoPlay playsInline />
+      </section>
+      <section className="action-bar">
+        <CallStatusIndicator />
+        <button type="button" onClick={hangup}>
+          Hangup
+        </button>
+      </section>
+      <section className="remotestream-area">
+        <video id="remote-video" width={400} autoPlay playsInline />
+      </section>
+    </main>
   );
 }
 
@@ -78,16 +119,8 @@ function JoinView() {
       });
   }, []);
 
-  const {
-    callState,
-    setDevices,
-    setUsername,
-    setOpponent,
-    registerUsername,
-    tryCall,
-    acceptIncomingCall,
-    hangup,
-  } = useContext(GlobalContext);
+  const { callState, setDevices, setUsername, setOpponent, registerUsername, tryCall } =
+    useContext(GlobalContext);
 
   const handleRegisterSubmit = (event) => {
     event.preventDefault();
@@ -133,72 +166,59 @@ function JoinView() {
     );
   }
 
-  const mustShowCallControls = [
-    STATE_CALLING,
-    STATE_RINGING,
-    STATE_ANSWERING,
-    STATE_IN_CALL,
-  ].includes(callState);
-
   return (
-    <main>
-      {(callState === STATE_CONNECTED || callState === STATE_REGISTER_FAILED) && (
-        <form onSubmit={handleRegisterSubmit}>
-          <label>
-            My username:
-            <input name="username" autoComplete="off" defaultValue="Marcell" required />
-          </label>
-          <br />
-          <label>
-            Microphone:
-            <select name="microphone" required>
-              {microphones.map((mic) => (
-                <option key={mic.deviceId} value={mic.deviceId}>
-                  {mic.label}
-                </option>
-              ))}
-            </select>
-          </label>
-          <br />
-          <label>
-            Camera:
-            <select name="camera" required>
-              {cameras.map((cam) => (
-                <option key={cam.deviceId} value={cam.deviceId}>
-                  {cam.label}
-                </option>
-              ))}
-            </select>
-          </label>
-          <br />
-          <button>Register</button>
-        </form>
-      )}
-      {(callState === STATE_REGISTERED || callState === STATE_CALL_FAILED) && (
-        <form onSubmit={handleCallSubmit}>
-          <label>
-            {"Another player's username:"}
-            <input name="opponent" autoComplete="off" defaultValue="Rodrigo" required />
-          </label>
-          <br />
-          <button>Duel!</button>
-        </form>
-      )}
-      {callState === STATE_RINGING && (
-        <button type="button" onClick={acceptIncomingCall}>
-          Accept call
-        </button>
-      )}
-      {mustShowCallControls && (
-        <>
-          <button type="button" onClick={hangup}>
-            Hangup
-          </button>
-          <br />
-          <video id="local-video" width={400} autoPlay playsInline />
-          <video id="remote-video" width={400} autoPlay playsInline />
-        </>
-      )}
-    </main>
+    <>
+      <main>
+        {(callState === STATE_CONNECTED || callState === STATE_REGISTER_FAILED) && (
+          <form onSubmit={handleRegisterSubmit}>
+            <label>
+              My username:
+              <input name="username" autoComplete="off" defaultValue="Marcell" required />
+            </label>
+            <br />
+            <label>
+              Microphone:
+              <select name="microphone" required>
+                {microphones.map((mic) => (
+                  <option key={mic.deviceId} value={mic.deviceId}>
+                    {mic.label}
+                  </option>
+                ))}
+              </select>
+            </label>
+            <br />
+            <label>
+              Camera:
+              <select name="camera" required>
+                {cameras.map((cam) => (
+                  <option key={cam.deviceId} value={cam.deviceId}>
+                    {cam.label}
+                  </option>
+                ))}
+              </select>
+            </label>
+            <br />
+            <button>Register</button>
+          </form>
+        )}
+        {(callState === STATE_REGISTERED || callState === STATE_CALL_FAILED) && (
+          <form onSubmit={handleCallSubmit}>
+            <label>
+              {"Another player's username:"}
+              <input name="opponent" autoComplete="off" defaultValue="Rodrigo" required />
+            </label>
+            <br />
+            <button>Duel!</button>
+          </form>
+        )}
+      </main>
+      <footer>
+        <p>
+          <small>
+            <CallStatusIndicator />
+          </small>
+        </p>
+      </footer>
+    </>
   );
 }
