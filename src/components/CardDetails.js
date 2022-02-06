aa5bf776268ad1a55f72b348288e26c3c4193d63
Rodrigo Muniz <rcmuniz51@gmail.com>
2022-02-06 01:24:05 -0300

    show details about card

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import get from 'lodash.get';
+import { Tabs, Tab } from 'react-bootstrap';
 
 export default function CardDetails() {
   const [cardData, setCardData] = useState({});
+  const [selectedTab, setSelectedTab] = useState('image');
 
   const retrieveCardDetails = (event) => {
     event.preventDefault();
@@ -16,15 +18,43 @@ export default function CardDetails() {
 
   return (
     <>
-      <form onSubmit={retrieveCardDetails}>
+      <form className="d-flex" onSubmit={retrieveCardDetails}>
         <input name="cardName" />
         <button type="submit">Pesquisar</button>
       </form>
       {cardData.name && (
-        <>
-          <br />
-          <img src={get(cardData, 'card_images.0.image_url', '')} alt={cardData.name} />
-        </>
+        <Tabs
+          className="m-1"
+          id="card-details"
+          activeKey={selectedTab}
+          onSelect={(k) => setSelectedTab(k)}
+          unmountOnExit
+        >
+          <Tab eventKey="image" title="Imagem">
+            <img src={get(cardData, 'card_images.0.image_url', '')} alt={cardData.name} />
+          </Tab>
+          <Tab className="d-flex flex-column" eventKey="details" title="Detalhes">
+            <span>
+              <strong>{cardData.name}</strong>
+            </span>
+            <span>
+              <strong>Ataque: </strong>
+              {cardData.atk}
+            </span>
+            <span>
+              <strong>Defesa: </strong>
+              {cardData.def}
+            </span>
+            <span>
+              <strong>Atributo: </strong>
+              {cardData.attribute}
+            </span>
+            <span>
+              <strong>Descrição: </strong>
+              {cardData.desc}
+            </span>
+          </Tab>
+        </Tabs>
       )}
     </>
   );
